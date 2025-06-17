ee3346800d666913c74b99360887e4d605b2fa08
Hanan Gershon Cohen <hanang.cohen@gmail.com>
2025-06-17 17:50:45 +0300

    add commits on class Factory.

--- src/GameObject/Factory.cpp
+++ src/GameObject/Factory.cpp
@@ -1,15 +1,22 @@
 #include "GameObject/Factory.h"
 
-std::map<char, Factory::CreatorFunc>& Factory::getMap() {
+// return map of Factory.
+std::map<char, Factory::CreatorFunc>& Factory::getMap() 
+{
     static std::map<char, CreatorFunc> map;
     return map;
 }
 
+// bool unimportant !
+// Just because the classes that implement it have a variable that is STATIC.
+// And it needs to be a certain TYPE , and we randomly chose it to be bool .
 bool Factory::registerIt(char symbol, CreatorFunc func) {
     getMap()[symbol] = func; 
     return true;
 }
 
+
+// if exist object in map , Return a constructed and ready-to-use object !
 std::unique_ptr<Object> Factory::create(char symbol, sf::Vector2f location, const ImagesObject& images) {
     auto it = getMap().find(symbol);
     if (it != getMap().end())
