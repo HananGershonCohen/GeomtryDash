43b3d50e2fee33e16ecb59181cc3d6e22570bac8
Hanan Gershon Cohen <hanang.cohen@gmail.com>
2025-06-19 17:56:30 +0300

    before change Move logical.

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,3 @@
-//#include "GameObject/StaticObject/StaticObject.h"
-//#include <SFML/Graphics.hpp>
-//#include "GameObject/Images/TypeObject.h"
-//#include "Information.h"
-//#include "GameObject/MovingObject/Player.h"
-//#include <iostream>
-//
-//int main()
-//{
-//	sf::Texture texture;
-//	if (!texture.loadFromFile("Robot.png")) {
-//		// Handle error
-//		return -1;
-//	}
-//	sf::Sprite sprite(texture);
-//	sprite.setScale(0.08, 0.08);
-//	sf::Vector2f loc(100.f, 550.f);
-//
-//	TypeObject type = TypeObject::player;
-//	/*StaticObject staticObj(loc, sprite, type);
-//	staticObj.p();
-//	Information(7);*/
-//	Player player(loc, sprite, type);
-//	
-//
-//	sf::Clock clock;
-//	float deltaTime = 0.0f;
-//	sf::RenderWindow window(sf::VideoMode(800, 600), "Game Window");
-//	window.setFramerateLimit(60); // Set the frame rate limit
-//
-//	while (window.isOpen())
-//	{
-//		sf::Event event;
-//		while (window.pollEvent(event))
-//		{
-//			if (event.type == sf::Event::Closed)
-//				window.close();
-//		}
-//
-//		deltaTime = clock.restart().asSeconds(); // Get the time elapsed since the last frame
-//		player.move(deltaTime); // Example deltaTime, adjust as needed
-//		
-//
-//		window.clear();
-//		player.draw(window);
-//		window.display();
-//	}
-//}
-//	
-//
 #include "GameController.h"
 #include "GameMenu/FontHolder.h"
 #include "GameMenu/ImagesButtons.h"
@@ -79,3 +29,79 @@ int main()
 	
 	
 }
+
+//#include <SFML/Graphics.hpp>
+//
+//int main()
+//{
+//    // יצירת חלון
+//    sf::RenderWindow window(sf::VideoMode(800, 600), "Gravity & Jump Example");
+//    window.setFramerateLimit(60);
+//
+//    // יצירת שחקן
+//    sf::RectangleShape player(sf::Vector2f(50.f, 50.f));
+//    player.setFillColor(sf::Color::Red);
+//    player.setPosition(100.f, 500.f); // התחל בגובה הרצפה
+//
+//    // משתנים לפיזיקה
+//	float velocityY = 0.f;  // מהירות אנכית של השחקן
+//    const float gravity = 1500.f;   //כח הכבידה / כח המשיכה
+//    const float jumpSpeed = -800.f; // ( מהירות התחלתית של הקפיצה ( שלילית = למעלה  
+//    const float groundLevel = 500.f;
+//    bool onGround = true;
+//
+//    sf::Clock clock;
+//
+//    while (window.isOpen())
+//    {
+//        // זמן שחלף מאז הפריים האחרון
+//        float deltaTime = clock.restart().asSeconds();
+//
+//        // אירועים
+//        sf::Event event;
+//        while (window.pollEvent(event))
+//        {
+//            if (event.type == sf::Event::Closed)
+//                window.close();
+//        }
+//
+//        // קפיצה (רק אם על הקרקע)
+//        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && onGround)
+//        {
+//            velocityY = jumpSpeed;
+//            onGround = false;
+//        }
+//
+//        // עדכון פיזיקה
+//        if (!onGround)
+//        {
+//			velocityY += gravity * deltaTime; // -800 --> 0 --> +800 (כח המשיכה מוסיף מהירות כל הזמן)
+//         
+//			/* another way to update position
+//            float posY = player.getPosition().y;
+//            posY += velocityY * deltaTime;
+//            player.setPosition(player.getPosition().x, posY);*/
+//
+//
+//            // valocityY. בהתבסס על 
+//            // ככל שהערך יותר רחוק מ0 האובייקט ינוע יותר מהר . 
+//            //וזה מה שגורם לאובייקט לעלות מהר בהתחלה ולהאט בהגיעו לסיום הקפיצה
+//            player.move(0.f, velocityY * deltaTime);
+//        }
+//
+//        // בדיקת רצפה
+//        if (player.getPosition().y >= groundLevel)
+//        {
+//            player.setPosition(player.getPosition().x, groundLevel);
+//            velocityY = 0.f;
+//            onGround = true;
+//        }
+//
+//        // ציור
+//        window.clear(sf::Color::White);
+//        window.draw(player);
+//        window.display();
+//    }
+//
+//    return 0;
+//}
